8eaefc6a28945463f085f933dfda52e091bfeb06
naimish <naimishjvora>
2023-01-17 13:25:54 +0530

    303 Fixed natural spelling typo

--- backend/src/oe/api/routes.ts
+++ backend/src/oe/api/routes.ts
@@ -8,7 +8,7 @@ import websocketHandler from '../../api/websocket-handler';
 import opEnergyApiService from './op-energy.service';
 import opEnergyWebsocket from './websocket';
 import opStatisticService from './op-statistics.service';
-import { isValidNeutralNumber, isValidPositiveNumber } from '../util/helper';
+import { isValidNaturalNumber, isValidPositiveNumber } from '../util/helper';
 
 class OpEnergyRoutes {
   public setUpHttpApiRoutes(app) {
@@ -223,7 +223,7 @@ class OpEnergyRoutes {
       if (
         !isValidPositiveNumber(startBlockHeight) ||
         !isValidPositiveNumber(span) ||
-        !isValidNeutralNumber(numberOfSpan)
+        !isValidNaturalNumber(numberOfSpan)
       ) {
         throw Error('Not a valid input parameters.');
       }
--- backend/src/oe/util/helper.ts
+++ backend/src/oe/util/helper.ts
@@ -3,6 +3,6 @@ export const isValidPositiveNumber = (numberString: string): boolean => {
   return parseInt(numberString) > 0;
 };
 
-export const isValidNeutralNumber = (numberString: string): boolean => {
+export const isValidNaturalNumber = (numberString: string): boolean => {
   return parseInt(numberString) >= 0;
 };
