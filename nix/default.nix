42ab5c9fa10cf87976c9bd886ec6ed1e7aaccdc7
Dambaev Alexander <ice.redmine@gmail.com>
2023-03-01 14:30:18 +0800

    fix: pin nixpkgs in the overlay.nix such that it will be pinned both for nix-builds and for nixos-rebuild

--- nix/default.nix
+++ nix/default.nix
@@ -1,12 +1,10 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/0f8f64b54ed07966b83db2f20c888d5e035012ef.tar.gz";
-  pkgs = import nixpkgs {
+  pkgs = import <nixpkgs> {
     config = {};
     overlays = [
       (import ./overlay.nix)
     ];
   };
-
 in {
   op-energy-backend = pkgs.op-energy-backend;
   op-energy-frontend = pkgs.op-energy-frontend;
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,4 +1,14 @@
-self: super: {
-  op-energy-backend = (self.callPackage ./derivation.nix {}).op-energy-backend;
-  op-energy-frontend = (self.callPackage ./derivation.nix {}).op-energy-frontend;
+self: super:
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/0f8f64b54ed07966b83db2f20c888d5e035012ef.tar.gz";
+  pkgs = import nixpkgs {
+    config = {};
+    overlays = [ ];
+  };
+  args = {
+    pkgs = pkgs;
+  };
+in{
+  op-energy-backend = (pkgs.callPackage ./derivation.nix args).op-energy-backend;
+  op-energy-frontend = (pkgs.callPackage ./derivation.nix args).op-energy-frontend;
 }
